c6f0b773423138fa913e986045c969c3a6d488cf
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-21 17:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ const winPatterns = [
 ];
 
 document.getElementById("submit").addEventListener("click", () => {
-  player1 = document.getElementById("player-1").value.trim();
-  player2 = document.getElementById("player-2").value.trim();
+  player1 = document.getElementById("player1").value.trim();
+player2 = document.getElementById("player2").value.trim();
+
 
   if (player1 && player2) {
     document.getElementById("setup").style.display = "none";
